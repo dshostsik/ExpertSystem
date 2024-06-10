ea246caa29d8794c853ff2f7e02e271e4525ca04
dshostsik <248680@edu.p.lodz.pl>
2024-06-10 18:15:28 +0200

    testing tensorflow

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-from ucimlrepo import fetch_ucirepo
+from database import database
+from tensorflowTest import tensor
 
-# fetch dataset
-wine = fetch_ucirepo(id=109)
-
-# data (as pandas dataframes)
-X = wine.data.features
-y = wine.data.targets
-
-# metadata
-print(wine.metadata)
-
-# variable information
-print(wine.variables)
+database()
+tensor()
